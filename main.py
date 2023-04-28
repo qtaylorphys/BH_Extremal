1c3f998998174018d5a996fc16dcfcfc36b3941c
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2023-04-28 19:16:38 +0200

    Using python numba to speed up the code

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from scipy import interpolate
 
 from numbers import Real
 
-import pandas as pd
+import numba as nb
+
+from interpolate import cubic_spline
 
 def CDF(x: Real) -> Real:
     if isinstance(x, (list, np.ndarray)): x = x[0]
@@ -35,41 +37,36 @@ def find_x_from_CDF(val: Real) -> Real:
 
     return res.x[0]
 
-if __name__ == "__main__":
-    rng = np.random.default_rng(seed=42)
-    cdffunction = pd.read_csv('cdffunction.csv', header = None)
-    
-    y_points = np.linspace(0.000001, 12, 10001)
-
-    def dist(x):
-        invfunction = interpolate.splrep(cdffunction, y_points)
-        return interpolate.splev(x, invfunction)
-    
-    init_M = 100
-    M = init_M
-
-    init_J = 0
-    J = init_J
-
+@nb.njit(
+    cache = True,
+    fastmath = True,
+)
+def compute_BH_evolution(
+    M_init,
+    J_init,
+    eps,
+    dist,
+):
+    M = M_init
+    J = J_init
     i = 0
 
-    eps = 1e-4
-    
-    while M >= 1:
+    while M >= 1.:
         print(i)
 
-        a_star = J / M**2
+        a_star = J / M**2.
         print(M, J, a_star)
         print()
-        if np.abs(a_star) > 1 - eps:
-            a_star = 1
+        if np.abs(a_star) > 1. - eps:
+            a_star = 1.
             break
 
-        rho = 1 / 2 * (1 - 2 * a_star)**2
-        T = 1 / (8 * np.pi * M)
+        rho = 1. / 2. * (1. - 2. * a_star)**2.
 
-        probs = rng.random()
-        rands = rng.random()
+        T = np.sqrt(1 - a_star**2) / (8 * np.pi * M * (1 + np.sqrt(1 - a_star**2)))
+
+        probs = np.random.rand()
+        rands = np.random.rand()
 
         change = dist(probs)
 
@@ -81,5 +78,23 @@ if __name__ == "__main__":
             J = J + change * T
 
         i += 1
+
+if __name__ == "__main__":
+    cdffunction = np.genfromtxt("cdffunction.csv")
+    y_points = np.linspace(0.000001, 12, 10001)
+
+    # def dist(x):
+    #     invfunction = interpolate.splrep(cdffunction, y_points)
+    #     return interpolate.splev(x, invfunction)
+    
+    inv_CDF = cubic_spline(cdffunction, y_points)
+    
+    init_M = 1000.
+    init_J = 0.
+    eps = 1e-4
+
+    compute_BH_evolution(init_M, init_J, eps, inv_CDF)
+    
+    
             
 
