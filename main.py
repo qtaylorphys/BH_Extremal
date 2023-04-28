4f569bdf389b1fc2c7800eece4c0d837ba63df28
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2023-04-28 18:35:08 +0200

    Code improvement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mpmath import polylog, zeta
 from scipy.optimize import root
+from scipy import interpolate
 
 from numbers import Real
 
@@ -37,60 +38,12 @@ def find_x_from_CDF(val: Real) -> Real:
 if __name__ == "__main__":
     rng = np.random.default_rng(seed=42)
     cdffunction = pd.read_csv('cdffunction.csv', header = None)
-    from scipy import interpolate
-    y_points = np.linspace(0.000001,12,10001)#Number is 10001 instead of 10k due to indexing differences between mathematica and python
+    
+    y_points = np.linspace(0.000001, 12, 10001)
 
     def dist(x):
         invfunction = interpolate.splrep(cdffunction, y_points)
         return interpolate.splev(x, invfunction)
-    
-    N = 49000 #Number of time steps
-    Nmc = 1000 #Number of Monte Carlo Sims (number of black holes)
-    prob = rng.random(size = (Nmc,N))
-    # change = dist(prob)
-
-    mass_i = 100 #in Planck Masses
-    Mass = np.zeros((Nmc+1,N+1))
-    J = np.zeros((Nmc+1,N+1))
-    astar = np.zeros((Nmc+1,N+1))
-    Mass[:,0] = mass_i
-    rand = rng.random(size = (Nmc,N))
-
-    for n in range(Nmc):
-        print("BH ", n)
-        for i in range(N):
-            # print(i)
-            j = J[n,i]
-            m = Mass[n,i]
-            # print(j, m)
-            if m < 1:
-                Mass[n,i:] = 1
-                break
-            else:
-                astar[n,i] = abs(j/m/m)
-                if abs(j/m/m) > 1:
-                    astar[n,i-1:] = 1
-                    J[n,i-1:] = j
-                    Mass[n,i-1:] = m
-                    break
-                rho = 1/2*(1-2*j/m**2)**2
-                temp = 1/8/np.pi/m #schwarzchild temperature
-
-                change_new = dist(prob[n, i])
-
-                Mass[n,i+1] = m-change_new*temp 
-                if rho > rand[n,i]:
-                    J[n,i+1] = j-change_new*temp
-                else:
-                    J[n,i+1] = j+change_new*temp
-
-                if i == 46424 or i == 46425 or i == 46426:
-                    print(rho, temp, change_new, m, Mass[n,i], j, J[n,i], prob[n, i], rand[n,i])
-        
-        break
-
-    J_old = J[0, :]
-    M_old = Mass[0, :]
     
     init_M = 100
     M = init_M
@@ -98,28 +51,27 @@ if __name__ == "__main__":
     init_J = 0
     J = init_J
 
-    n = 0
     i = 0
-    print()
+
+    eps = 1e-4
+    
     while M >= 1:
-        # print(i)
-        # print(np.abs(J_old[i] - J))
-        # print(np.abs(M_old[i] - M))
+        print(i)
 
         a_star = J / M**2
-        if a_star > 1:
+        print(M, J, a_star)
+        print()
+        if np.abs(a_star) > 1 - eps:
             a_star = 1
             break
 
         rho = 1 / 2 * (1 - 2 * a_star)**2
         T = 1 / (8 * np.pi * M)
 
-        probs = prob[n, i]
-        rands = rand[n, i]
-        change = dist(probs)
+        probs = rng.random()
+        rands = rng.random()
 
-        if i == 46424 or i == 46425 or i == 46426:
-            print(rho, T, change, M, M_old[i], J, J_old[i], prob[n, i], rand[n, i])
+        change = dist(probs)
 
         M = M - change * T
 
@@ -129,11 +81,5 @@ if __name__ == "__main__":
             J = J + change * T
 
         i += 1
-
-
             
-    #Mass[Mass < 1] = 1;
-    #astar[astar > 1] = 1;
-    # Mass = np.delete(Mass, -1, 0)
-    # J = np.delete(J, -1, 0)
-    # astar = np.delete(astar,-1,0)
+
