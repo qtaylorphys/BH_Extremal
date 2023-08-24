f8718ebfb58e204dd62380ec06bbb34123afd2b8
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2023-08-24 22:31:19 +0200

    Remove functions that go to utils.py

--- CDF.py
+++ CDF.py
@@ -4,13 +4,11 @@ from mpmath import polylog, zeta, mp
 from scipy.optimize import root
 import scipy.interpolate as spip
 from time import process_time
-import h5py
 import os
 
 from dateutil.relativedelta import relativedelta as rd
 
 from numbers import Real
-from nptyping import NDArray
 
 
 @np.vectorize
@@ -76,37 +74,6 @@ def find_x_from_CDF(val: Real) -> Real:
     )
 
     return res.x[0]
-
-def binary_size(num: Real, suffix: str = "B") -> str:
-    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
-        if abs(num) < 1024.0:
-            return f"{num:3.1f} {unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.1f} Yi{suffix}"
-
-def time_fmt(time_s: Real) -> str:
-    intervals = ["days", "hours", "minutes", "seconds"]
-    x = rd(seconds=time_s)
-    vals = [getattr(x, k) for k in intervals]
-    
-    if not all(v == 0 for v in vals[:-1]):
-        vals = [int(v) for v in vals]
-    else:
-        vals[-1] = np.round(vals[-1], 1)
-
-    return ' '.join(
-        f"{val} {interval}"
-        for val, interval in zip(vals, intervals) if val != 0
-    )
-
-def save_data_h5(data: NDArray, filename: str, dataset_name: str) -> None:
-    with h5py.File(filename, 'w') as f:
-        f.create_dataset(
-            dataset_name,
-            data=data,
-            compression="gzip",
-            compression_opts=9,
-        )
 
 
 if __name__ == "__main__":
