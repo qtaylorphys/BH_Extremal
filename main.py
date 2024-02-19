d2ec83c935dec3ed5e4db266780b556e8919d7fe
Deyan Mihaylov <d.p.mihaylov@gmail.com>
2024-02-19 23:54:57 +0100

    Remove unnecessary code

--- main.py
+++ main.py
@@ -22,17 +22,6 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--eps", type=float, default=1.)
     args = parser.parse_args()
 
-    CDF_data = load_CDF_data(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "CDF_data.h5",
-    ))
-    CDF_vals = CDF_data[:, 0]
-    x_vals = CDF_data[:, 1]
-
-    inv_CDF_interp = spip.CubicSpline(CDF_vals, x_vals)
-
-    new_cs = cubic_spline(CDF_vals, x_vals)
-
     pbh = PrimordialBlackHole(
         args.spacetime,
         args.initial_mass, args.final_mass,
@@ -40,40 +29,20 @@ if __name__ == "__main__":
         args.eps,
     )
 
-    N_PBH = 100
+    N_PBH = 10000
     zfill_len = int(np.log10(N_PBH)) + 1
 
     for i in range(N_PBH):
-        print(i)
-        # rands = np.random.uniform(
-        #     size=N,
-        #     low=CDF_vals[0],
-        #     high=CDF_vals[-1],
-        # )
-
-        # t1 = process_time()
-        # changes_array = inv_CDF_interp(rands)
-        # t2 = process_time()
-        # changes_array2 = compute_interp(new_cs, rands)
-
-        # print(np.max(np.abs(changes_array - changes_array2)))
-        # j = np.argmax(np.abs(changes_array - changes_array2))
-        # print(rands_array0[j], changes_array[j], changes_array2[j])
-        # print(t3 - t2, t2 - t1)
-        # print()
-        # print(t2 - t1)
-
-        # rands_array = np.random.rand(N)
+        if i % 1000 == 0:
+            print(i)
 
         pbh.evolve()
-
-        # t1 = process_time()
 
         with open(os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             f"results/test_M_{int(pbh.M_init)}.csv",
         ), "a") as f:
-            f.write(f"{pbh.M_init},{pbh.J_init},{pbh.M_end},{pbh.J_end},{pbh.a_star_end},{pbh.n_steps},{pbh.computation_time:.3e}\n")
+            f.write(f"{pbh.M_init},{pbh.J_init},{pbh.M_end},{pbh.J_end},{pbh.a_star_end},{pbh.n_steps},{int(pbh.extremal)},{pbh.computation_time:.3e}\n")
 
             if pbh.path is not None:
                 with h5py.File(f"./path{str(i).zfill(zfill_len)}.h5", 'w') as f:
